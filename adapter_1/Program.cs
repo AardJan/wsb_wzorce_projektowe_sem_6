fa452cdfdd0e79d63d0924727adb57375661bc81
AardJan <43217550+AardJan@users.noreply.github.com>
2024-03-24 09:44:25 +0100

    chore: add adapter_1 example code

---
+++ adapter_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AdapterSquare
+{
+    public interface ITarget
+    {
+        string GetArea();
+    }
+
+    class Square
+    {
+        public int  ()
+        {
+            return 3;
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        private readonly Square _adaptee;
+
+        public Adapter(Square adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetArea()
+        {
+            int side = this._adaptee.GetSideLength();
+            return $"Area is {side * side}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine(target.GetArea());
+        }
+    }
+}
